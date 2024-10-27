869d37205d6646b88121f81ba048f8e988ad949e
Dipali <dipalishinde560@gmail.com>
2024-10-27 20:33:03 +0530

    Task 1

---
+++ Gallery.js
@@ -0,0 +1,10 @@
+
+var fullImageBox  = document.getElementById("fullImageBox");
+var fullImage = document.getElementById("fullImage");
+
+function open (pic) {
+    fullImageBox.style.display="flex";
+    // fullImage.src =pic;  
+}
+
+   
